19322923e8ec720dd2f301c08606be80e85bc4ed
Gseganzerla <g.seganzerla@gmail.com>
2021-05-05 23:41:23 -0300

    2 9

--- resources/views/admin/pages/plans/index.blade.php
+++ resources/views/admin/pages/plans/index.blade.php
@@ -3,45 +3,55 @@
 @section('title', 'Planos')
 
 @section('content_header')
-    <h1>Planos <a href="{{ route('plans.create') }}" class="btn btn-dark">Add</a></h1>
+<ol class="breadcrumb">
+    <li class="breadcrumb-item"><a href="{{ route('admin.index') }}">Dashboard</a></li>
+    <li class="breadcrumb-item active"><a href="{{ route('plans.index') }}">Planos</a></li>
+</ol>
+
+<h1>Planos <a href="{{ route('plans.create') }}" class="btn btn-dark">Add</a></h1>
 @endsection
 
 @section('content')
-    <div class="card">
-        <div class="card-header">
-            <form action="{{ route('plans.search') }}" method="post" class="form form-inline">
-                @csrf
-                <input type="text" class="form-control" name="filter" planceholder="Nome" value="{{$filters['filter'] ?? ''}}">
-                <button type="submit" class="btn btn-dark">Filtrar</button>
-            </form>
-        </div>
-        <div class="card-body">
-            <table class="table table-condensed">
-                <thead>
-                    <tr>
-                        <th>Nome</th>
-                        <th>Preço</th>
-                        <th width="50px">Ações</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($plans as $plan)
-                        <tr>
-                            <td>{{ $plan->name }}</td>
-                            <td>R$ {{ number_format($plan->price, 2, ',', '.') }}</td>
-                            <td style="width=50px;"><a href="{{ route('plans.show', $plan->url) }}" class="btn btn-warning">Ver</a></td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
-        <div class="card-footer">
-            @if (isset($filters))
-            {{ $plans->appends($filters)->links() }}
-            @else
-            {{ $plans->links() }}
-            @endif
-        </div>
+<div class="card">
+    <div class="card-header">
+        <form action="{{ route('plans.search') }}" method="post" class="form form-inline">
+            @csrf
+            <input type="text" class="form-control" name="filter" planceholder="Nome"
+                value="{{ $filters['filter'] ?? '' }}">
+            <button type="submit" class="btn btn-dark">Filtrar</button>
+        </form>
     </div>
+    <div class="card-body">
+        <table class="table table-condensed">
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Preço</th>
+                    <th width="50px">Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($plans as $plan)
+                    <tr>
+                        <td>{{ $plan->name }}</td>
+                        <td>R$
+                            {{ number_format($plan->price, 2, ',', '.') }}
+                        </td>
+                        <td style="width=50px;"><a
+                                href="{{ route('plans.show', $plan->url) }}"
+                                class="btn btn-warning">Ver</a></td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+    <div class="card-footer">
+        @if(isset($filters))
+            {{ $plans->appends($filters)->links() }}
+        @else
+            {{ $plans->links() }}
+        @endif
+    </div>
+</div>
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,5 @@ Route::get('admin/plans/{url}', 'Admin\PlanController@show')->name('plans.show')
 Route::get('admin/plans', 'Admin\PlanController@index')->name('plans.index');
 Route::post('admin/plans', 'Admin\PlanController@store')->name('plans.store');
 Route::get('admin/plans/create', 'Admin\PlanController@create')->name('plans.create');
+
+Route::get('admin', 'Admin\PlanController@create')->name('admin.index');
