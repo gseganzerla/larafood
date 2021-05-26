64501df3e5fa9285aee863e52c31932b50ad119f
Gseganzerla <g.seganzerla@gmail.com>
2021-05-26 01:31:21 -0300

    18 2

--- app/Models/Traits/UserACLTrait.php
+++ app/Models/Traits/UserACLTrait.php
@@ -3,15 +3,42 @@
 
 namespace App\Models\Traits;
 
+use App\Models\Tenant;
 
 trait UserACLTrait
 {
+    private $permissions;
+
     public function permissions()
     {
-        $tenant = $this->tenant()->first();
-        $plan = $tenant->plan;
 
-        $permissions = [];
+        $permissionsPlan = $this->permissionsPlan();
+        $permissionsRole = $this->permissionsRole();
+
+        foreach ($permissionsRole as $permissionRole) {
+            if (in_array($permissionRole, $permissionsPlan)) {
+                array_push($permissions, $permissionsPlan);
+            }
+        }
+
+        return $permissions;
+    } 
+
+    public function permissionsRole()
+    {
+        $roles = $this->roles()->with('permissions')->get();
+
+        foreach ($roles->permissions as $permission) {
+            array_push($permissions, $permission->name);
+        }
+
+        return $permissions;
+    }
+
+    public function permissionsPlan()
+    {
+        $tenant = Tenant::with('plan.profiles.permissions')->where('id', $this->tanant_id)->first();
+        $plan = $tenant->plan;
 
         foreach ($plan->profiles as $profile) {
             foreach ($profile->permissions as $permission) {
@@ -36,5 +63,4 @@ trait UserACLTrait
     {
         return !in_array($this->email, config('acl.admins'));
     }
-
 }
