4fb56b7b7ca57f87e620813ddba076208dc31486
Gseganzerla <g.seganzerla@gmail.com>
2021-05-29 00:07:58 -0300

    19 4

---
+++ app/Http/Controllers/Api/TenantApiController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Services\TenantService;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\TenantResource;
+
+class TenantApiController extends Controller
+{
+    public function __construct(TenantService $tenantService)
+    {
+        $this->tenantService = $tenantService;
+    }
+
+    public function index()
+    {
+        return
+            TenantResource::collection(
+                $this->tenantService->getAllTenants()
+            );
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,1 +1,5 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/tenants', 'Api\\TenantApiController@index');
