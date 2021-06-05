37cf2666afeed188b89bb97dbd88011d3dedfaa3
Gseganzerla <g.seganzerla@gmail.com>
2021-06-05 16:41:40 -0300

    20 3

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -2,9 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Sanctum\HasApiTokens;
 
-class Client extends Model
+class Client extends Authenticatable
 {
+    use HasApiTokens;
+
     protected $fillable = ['name', 'email', 'password'];
 }
