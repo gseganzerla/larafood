5d6682f5b010d713853e606b558f3375dddc05a3
Gseganzerla <g.seganzerla@gmail.com>
2021-06-08 20:55:51 -0300

    21 5

---
+++ database/migrations/2021_06_08_201937_create_orders_table.php
@@ -0,0 +1,65 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('identify')->unique();
+            $table->integer('client_id')->nullable();
+            $table->integer('table_id')->nullable();
+            $table->double('total', 10, 2);
+            $table->enum('status', ['open', 'done', 'working', 'canceled', 'delivering']);
+            $table->text('comment')->nullable();
+
+
+            $table->unsignedBigInteger('tenant_id');
+            $table->foreign('tenant_id')
+                ->references('id')
+                ->on('tenants')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+
+
+        Schema::create('order_product', function (Blueprint $table) {
+            $table->id();
+            $table->string('identify')->unique();
+            $table->integer('client_id')->nullable();
+            $table->integer('table_id')->nullable();
+            $table->double('total', 10, 2);
+            $table->enum('status', ['open', 'done', 'rejected', 'working', 'canceled', 'delivering']);
+            $table->text('comment')->nullable();
+            $table->double('price', 10, 2);
+            $table->integer('quantity');
+
+
+            $table->unsignedBigInteger('order_id');
+            $table->unsignedBigInteger('product_id'); 
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_product');
+        Schema::dropIfExists('orders');
+    }
+}
