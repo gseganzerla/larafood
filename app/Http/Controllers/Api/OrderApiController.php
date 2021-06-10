5099cdea3d9d9abae546d264ad9f91869a6ef9f1
Gseganzerla <g.seganzerla@gmail.com>
2021-06-09 23:22:03 -0300

    21 13

--- app/Http/Controllers/Api/OrderApiController.php
+++ app/Http/Controllers/Api/OrderApiController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\StoreOrder;
+use App\Http\Requests\Api\TenantFormRequest;
 use App\Http\Resources\OrderResource;
 use App\Services\OrderService;
 use Illuminate\Http\Request;
@@ -21,6 +22,17 @@ class OrderApiController extends Controller
     {
         $order = $this->orderService->createNewOrder($request->all());
 
+        return new OrderResource($order);
+    }
+
+    public function show($identify) 
+    {
+        $order = $this->orderService->getOrderByIdentify($identify);
+
+        if (!$order) {
+            return response()->json(['message' => 'Not Foundd']);
+        }
+        
         return new OrderResource($order);
     }
 }
